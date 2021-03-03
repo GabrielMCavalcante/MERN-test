1b620ee030a4b7fb850120dcd82ef7101e45aeb7
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2021-03-03 09:58:10 -0400

    Add cors headers

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios"
 
 let axiosInstance = axios.create({
-  baseURL: "https://mern-test-server.herokuapp.com"
+  baseURL: "https://mern-test-server.herokuapp.com",
+  headers: {
+    "Access-Control-Allow-Origin": "*"
+  }
 })
 
 export default axiosInstance
